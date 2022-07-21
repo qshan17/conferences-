156ddf4e8e6375d063ec02ba6bbcb6709a0a8777
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-07-21 16:21:31 +0000

    added jekyll

--- K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
@@ -4,4 +4,4 @@ set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "5.3.0")
 set(CMAKE_C_COMPILER_WRAPPER "")
 set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "11")
-set(CMAKE_C_COMPILE_FEA
+set(CMAKE_C_COMPILE_FEATURES "c_function_prototypes;c_restrict;c_variadic_macros;
