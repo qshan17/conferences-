8628eb3d376aabe3a05b9643524bf89cced6b0bf
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-07-24 15:33:31 +0000

    Bumped to version 0.2.0

--- K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
@@ -4,4 +4,5 @@ set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "5.3.0")
 set(CMAKE_C_COMPILER_WRAPPER "")
 set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "11")
-set(CMAKE_C_COMPILE_FEATURES "c_function_prototypes;c_restrict;c_variadic_macros;
+set(CMAKE_C_COMPILE_FEATURES "c_function_prototypes;c_restrict;c_variadic_macros;c_static_assert")
+set(CMAKE_C90_COMPILE_FEATURES "c_function_pr
