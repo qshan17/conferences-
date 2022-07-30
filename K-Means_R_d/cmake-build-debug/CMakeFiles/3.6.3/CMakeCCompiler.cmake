84450f704e8322c09aabbca346d41e5ec4c708cf
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-07-30 08:53:52 +0000

    ext2: Fix fs corruption in ext2_get_xip_mem()

--- K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
@@ -6,4 +6,5 @@ set(CMAKE_C_COMPILER_WRAPPER "")
 set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "11")
 set(CMAKE_C_COMPILE_FEATURES "c_function_prototypes;c_restrict;c_variadic_macros;c_static_assert")
 set(CMAKE_C90_COMPILE_FEATURES "c_function_prototypes")
-set(CMAKE_C99_COMPILE_FEATURES "c_restrict;c_v
+set(CMAKE_C99_COMPILE_FEATURES "c_restrict;c_variadic_macros")
+set(CMAKE_C11_COMPILE_FEATURES "c_static_as
