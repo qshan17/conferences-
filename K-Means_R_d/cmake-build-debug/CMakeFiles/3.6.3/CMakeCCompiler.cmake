9980d0c234ae353f96b141b25e7a75adf717c3a1
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-07-30 09:45:13 +0000

    delete example

--- K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
@@ -7,4 +7,7 @@ set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "11")
 set(CMAKE_C_COMPILE_FEATURES "c_function_prototypes;c_restrict;c_variadic_macros;c_static_assert")
 set(CMAKE_C90_COMPILE_FEATURES "c_function_prototypes")
 set(CMAKE_C99_COMPILE_FEATURES "c_restrict;c_variadic_macros")
-set(CMAKE_C11_COMPILE_FEATURES "c_static_as
+set(CMAKE_C11_COMPILE_FEATURES "c_static_assert")
+
+set(CMAKE_C_PLATFORM_ID "MinGW")
+set(CMAKE_
