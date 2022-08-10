3cd0aa83a5bc5b3c14de241850e0029fc1d251c8
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-08-10 16:32:10 +0000

    oauth2

--- K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
@@ -37,4 +37,5 @@ set(CMAKE_C_SOURCE_FILE_EXTENSIONS c;m)
 set(CMAKE_C_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
 set(CMAKE_C_LINKER_PREFERENCE 10)
 
-# Save compi
+# Save compiler ABI information.
+set(CMAKE_C_SIZEOF_DATA_PT
