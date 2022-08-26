ab55f2c8f072e4b41a9fb8aecbfeb985e24c7766
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-08-26 18:40:36 +0000

    Update index2.html

--- K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/3.6.3/CMakeCCompiler.cmake
@@ -62,4 +62,5 @@ endif()
 
 
 
-set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "mingw32;moldname;mingwex;msvcrt;advapi3
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "mingw32;moldname;mingwex;msvcrt;advapi32;shell32;user32;kernel32;mingw32;moldname;mingwex;msvcrt")
+set(CMAKE_C_IMP
