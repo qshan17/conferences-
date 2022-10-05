ca92f017eb0730195fce0ec6bbf2965b7aa5728f
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-10-05 22:00:17 +0000

    Renamed module

---
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/K_Means_R_d.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+
+file(REMOVE_RECURSE
+  "CMakeFiles/K_Means_R_d.dir/main.c.obj"
+  "CMakeFiles/K_Means_R_d.dir/utils.c.obj"
+  "K_Means_R_d.pdb"
+  "K_Means_R_d.exe"
+  "K_Means_R_d.exe.manifest"
+  "libK_Means_R_d.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/K_Means_R_d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
