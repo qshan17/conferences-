22f3599e93fe0a61a999b012f31f363ff13411bd
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-11-06 22:45:12 +0000

    Update Concepts.md

--- K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -9,4 +9,5 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeCInformation.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeCXXInformation.cmake"
-  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modul
+  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeCommonLanguageInclude.cmake"
+  "C:/Program Files (x86)/JetBrains/
