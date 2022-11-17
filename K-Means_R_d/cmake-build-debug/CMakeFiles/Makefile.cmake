1bdd6203496a5664b5cca1ada844cc0a9748abfe
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-11-17 09:27:38 +0000

    drm/radeon: fix logic error in atombios_encoders.c

--- K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -14,4 +14,5 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeFindCodeBlocks.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeGenericSystem.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeLanguageInformation.cmake"
-  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeRCInform
+  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeRCInformation.cmake"
+  "C:/Program Files (x86)/JetBrains/CLion 20
