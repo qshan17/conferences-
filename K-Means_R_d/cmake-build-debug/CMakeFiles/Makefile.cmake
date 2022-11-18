7e6306a081d613192717e6c52fcb0e3c05abd38d
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-11-18 14:15:39 +0000

    Rename templates

--- K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -16,4 +16,5 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeLanguageInformation.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeRCInformation.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeSystemSpecificInformation.cmake"
-  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cm
+  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/CMakeSystemSpecificInitialize.cmake"
+  "C:/Program Fil
