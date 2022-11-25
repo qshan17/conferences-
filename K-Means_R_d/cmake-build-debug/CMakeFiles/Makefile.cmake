205f20ff131967e50a4522ec4211373a360ba5d4
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-11-25 16:25:04 +0000

    change syntax

--- K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -20,4 +20,5 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Compiler/GNU-C.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Compiler/GNU-CXX.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Compiler/GNU.cmake"
-  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Win
+  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-GNU-C-ABI.cmake"
+  "C:/Program Files (x86)/JetBrains/CLio
