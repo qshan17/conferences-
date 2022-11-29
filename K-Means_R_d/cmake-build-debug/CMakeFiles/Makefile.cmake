75a843b6d5b45ecc258d0f80d317269b44bbb6b9
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-11-29 06:46:47 +0000

    Fix integration test

--- K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -23,4 +23,4 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-GNU-C-ABI.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-GNU-C.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-GNU-CXX-ABI.cmake"
-  "C:/Program Files (x86)/JetBrains/CLion 2016
+  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-GNU-CXX.cmake"
