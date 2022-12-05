cab384b67bee76b6027d7e2d015a7248e8f724f0
qshan17 <110629306+qshan17@users.noreply.github.com>
2022-12-05 18:57:16 +0000

    Add summary

--- K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ K-Means_R_d/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -26,4 +26,4 @@ set(CMAKE_MAKEFILE_DEPENDS
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-GNU-CXX.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-GNU.cmake"
   "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows-windres.cmake"
-  "C:/Program Files (x86)/JetBrains/CLion 201
+  "C:/Program Files (x86)/JetBrains/CLion 2016.3.2/bin/cmake/share/cmake-3.6/Modules/Platform/Windows.cmake"
