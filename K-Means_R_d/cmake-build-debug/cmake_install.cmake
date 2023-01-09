5186c51173bb9b0ffd04fa3216b9b082d1e6d3e0
qshan17 <110629306+qshan17@users.noreply.github.com>
2023-01-09 22:49:10 +0000

    Update spider.py

--- K-Means_R_d/cmake-build-debug/cmake_install.cmake
+++ K-Means_R_d/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,5 @@
 # Install script for directory: C:/Users/Marius/Desktop/K-Means_R_d
 
 # Set the install prefix
-if(NOT DEFINED CMAKE_INSTA
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/K
