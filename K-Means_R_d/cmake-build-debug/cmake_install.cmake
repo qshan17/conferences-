7c00769d4566f530b4ae02746ba1ebc3f7ebca85
qshan17 <110629306+qshan17@users.noreply.github.com>
2023-01-10 07:53:29 +0000

    [blacksmith] Bump version

--- K-Means_R_d/cmake-build-debug/cmake_install.cmake
+++ K-Means_R_d/cmake-build-debug/cmake_install.cmake
@@ -2,4 +2,6 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/K
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/K_Means_R_d")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE
