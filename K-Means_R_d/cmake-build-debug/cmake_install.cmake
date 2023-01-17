cb7b4fceb9dcafbc8b6fd794b50f6bd80a58380e
qshan17 <110629306+qshan17@users.noreply.github.com>
2023-01-17 12:21:33 +0000

    add handler

--- K-Means_R_d/cmake-build-debug/cmake_install.cmake
+++ K-Means_R_d/cmake-build-debug/cmake_install.cmake
@@ -12,3 +12,6 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Insta
