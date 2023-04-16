ff267c9c64616cd6208ba3eb2f48ecf44aefc904
qshan17 <110629306+qshan17@users.noreply.github.com>
2023-04-16 19:50:21 +0000

    msm: kgsl: Fix mem leak when page allocation fails

--- K-Means_R_d/utils.h
+++ K-Means_R_d/utils.h
@@ -17,3 +17,19 @@ struct Map{
 };
 
 struct States{
+    char **states;
+};
+
+struct Point get_Centre_Of_Mass(struct Point centroid, struct Point* points, int length, int d);
+struct Point get_Point_From_Line(char line[256]);
+
+float euclidian_Distance(struct Point p1, struct Point p2, int dimension);
+
+int get_Random(int number_Of_Points);
+int nearest_Centroid_From_Point(struct Point p1, struct Point* centroids, int k, int d);
+
+short int is_Point_One_Of_Centroids(struct Point *centroids, struct Point point, int c_Length, int d);
+
+void write_Last_Centroids_To_File(struct Point* centroids, const char* path, int number_Of_Centroids, int d);
+void initialize_Map(struct Map* map, int number_Of_Centroids);
+#endif //K_MEANS_R_D_UTILS_H
---
+++ README.md
@@ -0,0 +1,2 @@
+# Machine-Learning
+Machine Learning self-assigned projects
